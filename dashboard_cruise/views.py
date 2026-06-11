4c4fde0207a2fdafee401080474b43aa8e309bc0
Damian <damian.delgadogallardo01@gmail.com>
2026-06-11 15:19:09 -0400

    Add CSS style in Dashboard

--- dashboard_cruise/views.py
+++ dashboard_cruise/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from cruise.models import Cruise
+from room.models import Room
 
 def home(request):
-    return render(request, "home/dashboard.html")
+    cruises = Cruise.objects.all()
+    rooms = Room.objects.all()
+    return render(request, "home/base.html", {"cruises": cruises, "rooms": rooms})
